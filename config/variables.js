ab32d025a511150492a3ab20fd5f5315227bb8e4
Nicola Regattieri <nicregattieri@gmail.com>
2018-09-17 18:58:36 -0300

    working on the category shelf

--- config/variables.js
+++ config/variables.js
@@ -1,9 +1,9 @@
-const pjson = require('../package.json');
+const pjson = require("../package.json");
 
 module.exports = {
-  PROJECT_ID: "casaegaragem",
-  TEMPLATE_PREFIX: "eCore-",
+  PROJECT_ID: "thomsonreuters",
+  TEMPLATE_PREFIX: "eCore-ThomsonReuters",
   PROJECT_VERSION: pjson.version,
-  STORE_ID: "casaegaragem",
-  STORE_NAME: "Casa e Garagem"
-}
+  STORE_ID: "thomsonreuters",
+  STORE_NAME: "Thomson Reuters"
+};
--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -1,6 +1,4 @@
 
-
-
 /* $('.shelf__carousel--category ul').slick({
   arrows: true,
   slideToShow: 3,
@@ -12,74 +10,13 @@
   nextArrow:shelf__next
 }); */
 
-
 $(document).ready(function(){
   if($('body').hasClass('home')){
-    
-
-    if($('.call__box')[0]){
-      $('.call__box').each(function(){
-        const img = $('img', this).attr('src');
-
-        $(this).css('background-image', 'url('+img+')');
-      })
+    if($('li.helperComplement')[0]){
+      $('li.helperComplement').remove();
     }
-
-    $('.product--shelf-flip .product__front').on('click', function(){
-      $(this).parents('.product--shelf-flip').addClass('hover');
-    })
-
-    var $gallery = $('.banner--full .banner__inner');
-    var slideCount = null;
-
-    $gallery.on('init', function(event, slick){
-      slideCount = slick.slideCount;
-      setSlideCount();
-      setCurrentSlideNumber(slick.currentSlide);
+    $('.banner--full').slick({
     });
-
-    $gallery.on('beforeChange', function(event, slick, currentSlide, nextSlide){
-      setCurrentSlideNumber(nextSlide);
-    });
-
-    function setSlideCount() {
-      var $el = $('.slide-count-wrap').find('.total');
-      $el.text(function (i, n) {
-          var result = Number(n) + 1;
-          if ( result < 10 ) {
-              return "0" + slideCount;
-          } else {
-              return result;
-          }
-      })
-    }
-
-    function setCurrentSlideNumber(currentSlide) {
-      var $el = $('.slide-count-wrap').find('.current');
-      var n = currentSlide + 1;
-      $el.text(function (i, n) {
-          var result = currentSlide + 1;
-          if ( result < 10 ) {
-              return "0" + result;
-          } else {
-              return result;
-          }
-      });
-
-    }
-
-
-
-
-    $gallery.slick({
-      dots: true,
-      autoplay: true,
-      arrows: false,
-      fade: true,
-      infinite: false
-    });
-
-
 
     const shelf__prev = `<button type='button' class='slick-prev shelf__button'><svg data-name="Camada 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32.96 62.45"><path fill="#9e9e9e" d="M0 32.47l30.24 29.98 2.62-2.49L4.19 31.23 32.95 2.49 30.22 0 0 29.98v2.49z"/></svg></button>`
     const shelf__next = `<button type='button' class='slick-next shelf__button'><svg data-name="Camada 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32.96 62.45"><path fill="#9e9e9e" d="M32.95 29.98L2.72 0 .1 2.49l28.66 28.74L0 59.96l2.73 2.49 30.22-29.98v-2.49z"/></svg></button>`
@@ -90,39 +27,15 @@ $(document).ready(function(){
       slidesToScroll: 1,
       infinite: true,
       variableWidth: true,
-      prevArrow: shelf__prev,
-      nextArrow:shelf__next
     });
 
-    $('.shelf__brand').slick({
+    $('.category-nav ul').slick({
       arrows: true,
-      slideToShow: 4,
+      slideToShow: 5,
       slidesToScroll: 1,
-      infinite: true,
-      centerMode: true,
+      infinite: false,
       variableWidth: true,
-      prevArrow: shelf__prev,
-      nextArrow:shelf__next
+      variableHeight: true
     });
-
-    $(window).on('productFinished', function () {
-      console.log('productFinished');
-      $('.buy-by-category .shelf ul').each(function () {
-        if($('li.helperComplement', this)[0]){
-          $('li.helperComplement', this).remove();
-        }
-        if ($('li', this).length > 5 && !$(this).hasClass('slick-initialized')) {
-          $(this).slick({
-            arrows: true,
-            slideToShow: 3,
-            slidesToScroll: 1,
-            infinite: true,
-            variableWidth: true,
-            prevArrow: shelf__prev,
-            nextArrow:shelf__next
-          });
-        }
-      })
-    })
   }
 })
