827a465bd2735062ab0009c173b5927e81dbaa48
Nicola Regattieri <nicregattieri@gmail.com>
2018-09-27 18:08:13 -0300

    mobile header / banner/ shelf / containers

--- src/js/components/mobile-menu.js
+++ src/js/components/mobile-menu.js
@@ -1,19 +1,18 @@
 
 class MobileMenu {
   constructor() {
-    this.siteHeader = $(".main-header");
     this.menuIcon = $(".header__menu-icon");
-    this.menuContent = $(".main-header__menu-content");
+    this.menuMobileContent = $(".menu__mobile");
     this.events();
   }
 
   events(){
-    this.menuIcon.click(this.toggleTheMenu.bind(this));
+	this.menuIcon.click(this.toggleTheMenu.bind(this));
   }
 
+
   toggleTheMenu() {
-    this.menuContent.toggleClass("main-header__menu-content--is-visible");
-    this.siteHeader.toggleClass("main-header--is-expanded");
+    this.menuMobileContent.toggleClass("menu__mobile--is-visible");
     this.menuIcon.toggleClass("header__menu-icon--close-x");
   }
 }
