6462c29a813265c3827d3f68cb2de548c33f0a21
Nicola Regattieri <nicregattieri@gmail.com>
2018-09-28 19:27:00 -0300

    finished mobile

--- src/js/components/minicart.js
+++ src/js/components/minicart.js
@@ -14,7 +14,7 @@ class Minicart {
              <h4 class="minicart-product__name">${item.name}</h4>
              <strong class="minicart-product__price">R$ ${(item.price / 100).formatMoney()}</strong>
            </div>
-           
+
              <button class="minicart-product__remove" type="button" onclick="Minicart.removeItem.apply(null, [${i}])" title="Remover ${item.name} do carrinho">X</button>
          </li>
        `
@@ -30,7 +30,7 @@ class Minicart {
          <div class="minicart ${qty > 0 ? 'is-not-empty' : ''}">
             <button class="minicart__handle" title="sacola">
                 <span class="minicart__count">
-                    <i class="minicart__icon icon-cart"></i>
+                    <i class="minicart__icon simple-bag"></i>
                     <span class="minicart__count-value">${this.printQuantity(qty)}</span>
                 </span>
             </button>
@@ -48,8 +48,8 @@ class Minicart {
                     </ul>
                 </div>
                 <div class="minicart__footer">
-                    
-                        
+
+
                     <div class="minicart__totals">
                         <span class="minicart__totals-text">Total</span>
                         <strong class="minicart__totals-value">${this.getTotal()}</strong>
@@ -58,15 +58,15 @@ class Minicart {
                     Finalizar Pedido</a>
                 </div>
             </div>
-           
+
          </div>
        `
      }
-     
+
      removeItem(index) {
        vtexjs.checkout.removeItems([{index}])
      }
-     
+
      updateItem(obj) {
        let { index, qty, calc } = obj
        let quantity = qty + +calc
@@ -74,29 +74,29 @@ class Minicart {
          vtexjs.checkout.updateItems([{index, quantity}], null, false)
        }
      }
-     
+
      getTotal() {
        const itemsTotal = this.orderForm.totalizers.find(item => item.id === 'Items')
        const total = itemsTotal ? itemsTotal.value / 100 : 0
        return `R$ ${total.formatMoney()}`
      }
-     
+
      getQuantity() {
        const qty = this.orderForm.items.reduce((prev, next) => prev + next.quantity, 0)
        return qty
      }
-     
+
      printQuantity(qty) {
        return `${qty}`
      }
-     
+
      update(orderForm) {
        this.orderForm = orderForm
        this.mount()
      }
-     
+
      mount() {
-       $('.js-minicart').html(this.render())
+       $('.main-header__carrinho').html(this.render())
      }
 }
 
--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -1,70 +1,122 @@
-$(document).ready(function(){
-  if($('body').hasClass('home')){
-    if($('li.helperComplement')[0]){
+$(document).ready(() => {
+  if ($('body').hasClass('home')) {
+    if ($('li.helperComplement')[0]) {
       $('li.helperComplement').remove();
     }
-    $('.banner--full').slick({
+    $('.banner--full').slick({});
+
+    $('.shelf__banners').slick({
+      mobileFirst: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: true,
+      infinite: true,
+      mobileFirst: true,
+      responsive: [
+        {
+          breakpoint: 800,
+          settings: "unslick"
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
     });
 
-	$('.banner--mobile').slick({
-	  arrows: false,
-	  slidesToScroll: 1,
-	  infinite: true,
-	  dots: true,
-	  draggable: true,
-	});
-    const shelf__prev = `<button type='button' class='slick-prev shelf__button'><svg data-name="Camada 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32.96 62.45"><path fill="#9e9e9e" d="M0 32.47l30.24 29.98 2.62-2.49L4.19 31.23 32.95 2.49 30.22 0 0 29.98v2.49z"/></svg></button>`
-    const shelf__next = `<button type='button' class='slick-next shelf__button'><svg data-name="Camada 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32.96 62.45"><path fill="#9e9e9e" d="M32.95 29.98L2.72 0 .1 2.49l28.66 28.74L0 59.96l2.73 2.49 30.22-29.98v-2.49z"/></svg></button>`
+    $('.banner--mobile').slick({
+      arrows: false,
+      slidesToScroll: 1,
+      infinite: true,
+      dots: true,
+      draggable: true,
+    });
+    // const shelf__prev =			'<button type=\'button\' class=\'slick-prev shelf__button\'><svg data-name="Camada 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32.96 62.45"><path fill="#9e9e9e" d="M0 32.47l30.24 29.98 2.62-2.49L4.19 31.23 32.95 2.49 30.22 0 0 29.98v2.49z"/></svg></button>';
+    // const shelf__next =			'<button type=\'button\' class=\'slick-next shelf__button\'><svg data-name="Camada 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32.96 62.45"><path fill="#9e9e9e" d="M32.95 29.98L2.72 0 .1 2.49l28.66 28.74L0 59.96l2.73 2.49 30.22-29.98v-2.49z"/></svg></button>';
 
     $('.shelf__carousel--full ul').slick({
       arrows: true,
       slideToShow: 1,
       slidesToScroll: 1,
-      infinite: true,
-	  mobileFirst: true,
-	  responsive: [
-		{
-		  breakpoint: 1200,
-		  settings: {
-			slidesToShow: 4,
-			slidesToScroll: 2,
-			infinite: true,
-		  }
-		},
-		{
-		  breakpoint: 800,
-		  settings: {
-			slidesToShow: 3,
-			slidesToScroll: 3
-		  }
-		},
-		{
-		  breakpoint: 600,
-		  settings: {
-			slidesToShow: 2,
-			slidesToScroll: 2
-		  }
-		},
-		{
-		  breakpoint: 480,
-		  settings: {
-			slidesToShow: 1,
-			slidesToScroll: 1
-		  }
-		}
-		// You can unslick at a given breakpoint now by adding:
-		// settings: "unslick"
-		// instead of a settings object
-	  ]
+      mobileFirst: true,
+      dots: true,
+      infinite: false,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 2,
+            dots: false,
+            infinite: false,
+          },
+        },
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            dots: false,
+            infinite: true,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            infinite: false,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+            infinite: false,
+          },
+        },
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ],
     });
 
     $('.category-nav ul').slick({
       arrows: true,
-      slideToShow: 5,
+      slidesToShow: 2,
       slidesToScroll: 1,
-      infinite: false,
-      variableWidth: true,
-      variableHeight: true
+	  infinite: false,
+	  mobileFirst: true,
+	  dots: true,
+      responsive: [
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            dots: false,
+			infinite: false,
+			arrows: true
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 2,
+			infinite: false,
+			arrows: true
+          },
+        },
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ],
     });
   }
-})
+});
