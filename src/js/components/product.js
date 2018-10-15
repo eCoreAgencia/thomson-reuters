d3e0e5c90db8ef1a6765bc377244f26a0805da94
Nicola Regattieri <nicregattieri@gmail.com>
2018-10-15 16:05:15 -0300

    started product page

--- src/js/components/product.js
+++ src/js/components/product.js
@@ -25,11 +25,55 @@ if ($('body').hasClass('product')) {
   const shelf__next = `<button type='button' class='slick-next shelf__button'><svg data-name="Camada 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32.96 62.45"><path fill="#9e9e9e" d="M32.95 29.98L2.72 0 .1 2.49l28.66 28.74L0 59.96l2.73 2.49 30.22-29.98v-2.49z"/></svg></button>`
 
   $('.shelf__carousel--full ul').slick({
-    arrows: true,
-    slideToShow: 4,
-    slidesToScroll: 1,
-    infinite: true,
-    variableWidth: true
+	arrows: true,
+	slideToShow: 1,
+	slidesToScroll: 1,
+	mobileFirst: true,
+	dots: true,
+	infinite: false,
+	responsive: [
+	  {
+		breakpoint: 1024,
+		settings: {
+		  slidesToShow: 4,
+		  slidesToScroll: 2,
+		  dots: false,
+		  infinite: false,
+		},
+	  },
+	  {
+		breakpoint: 800,
+		settings: {
+		  slidesToShow: 3,
+		  slidesToScroll: 3,
+		  dots: false,
+		  infinite: true,
+		  arrows: false,
+		},
+	  },
+	  {
+		breakpoint: 600,
+		settings: {
+		  slidesToShow: 2,
+		  slidesToScroll: 2,
+		  infinite: false,
+		  arrows: false,
+		},
+	  },
+	  {
+		breakpoint: 480,
+		settings: {
+		  slidesToShow: 1,
+		  slidesToScroll: 1,
+		  dots: true,
+		  infinite: false,
+		  arrows: false,
+		},
+	  },
+	  // You can unslick at a given breakpoint now by adding:
+	  // settings: "unslick"
+	  // instead of a settings object
+	],
   });
 
   class Product {
