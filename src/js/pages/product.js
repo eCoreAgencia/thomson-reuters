14d551d122efd7b9245aa7b039909ddba6c7a643
Nicola Regattieri <nicregattieri@gmail.com>
2018-10-26 18:48:59 -0300

    fixes on QA

--- src/js/pages/product.js
+++ src/js/pages/product.js
@@ -1,33 +1,50 @@
 class Product {
-    constructor() {
-        if($('body').hasClass('product')) {
-            this.sumario();
-        }
+  constructor(link) {
+	this.renderSumario(link);
+	this.events();
+    if ($('body').hasClass('product')) {
+      this.sumario();
     }
+  }
+  sumario() {
+    const idProduct = $('input#___rc-p-id').attr('value');
 
-    sumario() {
-        let idProduct = $('input#___rc-p-id').attr('value');
+    $.ajax({
+      url: `/api/catalog_system/pub/products/search?fq=productId:${idProduct}`,
+      type: 'GET',
+      headers: {
+        Accept: 'application/vnd.vtex.masterdata.v10+json',
+        'Content-Type': 'application/vnd.vtex.masterdata.v10+json',
+      },
+    }).done((data) => {
+      const sumario = data[0].Sumário;
 
-        $.ajax({
-            url: "/api/catalog_system/pub/products/search?fq=productId:"+idProduct,
-            type: "GET",
-            headers: {
-                Accept: "application/vnd.vtex.masterdata.v10+json",
-                "Content-Type": "application/vnd.vtex.masterdata.v10+json"
-            }
-        }).done(function (data) {
-            let sumario = data[0].Sumário;
-            
-            if(typeof sumario === "undefined") {
-                $('a.button__preview').remove();
-            } else {
-                var link = sumario[0];
+      if (typeof sumario === 'undefined') {
+        $('.product__action-bottom--pgprod a.button__preview').remove();
+      } else {
+        const link = sumario[0];
+        $('.product__action-bottom--pgprod a.button__preview').attr('href', link);
+      }
+    });
+  }
 
-                $('a.button__preview').attr('href', link);
-                $('a.button__preview').attr('target', '_blank');
-            }
-        });
-    }
+  events() {
+    $('body').on('click', 'a.button__preview', (e) => {
+      e.preventDefault();
+      const link = $(e.target).attr('href');
+      this.renderSumario(link);
+      $('.modal-sumary').fadeIn();
+    });
+    $('body').on('click', '.modal-sumary__content .close', () => {
+      $('.modal-sumary').fadeOut();
+    });
+  }
+
+  renderSumario(link) {
+    const modalContent = `<iframe src="http://docs.google.com/gview?url=${link}&embedded=true"
+	style="" frameborder="0"></iframe>`;
+    $('.modal-sumary__container').html(modalContent);
+  }
 }
 
 window.filter = new Product();
