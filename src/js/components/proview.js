cdad999fca48819d41051c5dec7828fd6cd797ca
Felipe Pereira <lf.pereirarj@gmail.com>
2018-12-10 10:36:13 -0200

    ajustes

--- src/js/components/proview.js
+++ src/js/components/proview.js
@@ -8,7 +8,95 @@ class Proview {
 
 	fieldError(field) {
 		console.log(field);
-	}
+    }
+    
+    sendDataForm(data) {
+        let self = this;
+        const email = data.email;
+        const name = data.name;
+        const cupom = data.cupom;
+        
+        const user = self.getUserProview(email);
+
+        if(user) {
+            self.sendCupomToProview(user.id, cupom);
+
+        }else{
+            const newUser = self.createUserProview(email, name);
+            self.sendCupomToProview(newUser.id, cupom);
+        }
+
+    }
+
+    createUserProview(email, name) {
+        const endpoint = `/user/create`;
+        const settings = {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json; charset=utf-8'
+            },
+            method: 'POST',
+            body: {
+                email: email,
+                name: name
+            }
+        }
+
+        
+        return new Promise((resolve, reject) => {
+            return fetch(endpoint, settings)
+                    .then(data => {
+                        return resolve(data.json())
+                    })
+                    .catch(err => reject(err))
+        })
+    }
+
+    getUserProview(email) {
+        const endpoint = `/user/${email}`;
+        const settings = {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json; charset=utf-8'
+            }
+        }
+
+        
+        return new Promise((resolve, reject) => {
+            return fetch(endpoint, settings)
+                    .then(data => {
+                        return resolve(data.json())
+                    })
+                    .catch(err => reject(err))
+        })
+    }
+
+    sendCupomToProview(userId, cupom) {
+        const endpoint = `/user/code`;
+        const settings = {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json; charset=utf-8'
+            },
+            method: 'POST',
+            body: {
+                userId: userId,
+                cupom: cupom
+            }
+        }
+
+        
+        return new Promise((resolve, reject) => {
+            return fetch(endpoint, settings)
+                    .then(data => {
+                        return resolve(data.json())
+                    })
+                    .catch(err => reject(err))
+        })
+    }
+
+
+
 }
 
 
