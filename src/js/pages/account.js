aca320bb8829fe0d22ec4b6ad3abcec68b390069
Eduardo Dantas <dantasleld@gmail.com>
2018-12-20 14:57:22 -0200

    fix-alterada a opcao de cancelamento em meus pedidos

--- src/js/pages/account.js
+++ src/js/pages/account.js
@@ -6,14 +6,15 @@ class account {
 	}
 
 	hideCancel() {
-		$(document).ajaxStop(function () {
-			$('.myo-progress-bar__ma-inner-circle').each(function () {
+		setTimeout(function() {
+			$('.myo-progress-bar__ma-inner-circle').each(function() {
 				var _this = $(this);
 				var activeItem = _this.parent().find('.myo-progress-bar__text').text();
-				if (activeItem == "Entregar à transportadora" || activeItem == "Entregar pedido") {
+				console.log(activeItem)
+				if(activeItem == "Entregar à transportadora" || activeItem == "Entregar pedido" || activeItem == "Aprovando pagamento" || activeItem == "Preparar pedido") {
 					$('.myo-view-order ul.list li:last').remove();
 				}
 			})
-		})
+		}, 1500);
 	}
 }
