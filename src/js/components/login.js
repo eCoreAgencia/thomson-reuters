41e6e1a9220316047042279159656b0b0f340a05
Nicola Regattieri <nicregattieri@gmail.com>
2019-01-02 18:03:49 -0200

    header login iteraction / faltando troca de botoes mobile se logado / colocando autor em shelf /

--- src/js/components/login.js
+++ src/js/components/login.js
@@ -1,7 +1,27 @@
 $(document).ready(() => {
   if ($('body').hasClass('login')) {
-    $('body').on('click', '.vtexIdUI-page .close',  () => {
+    $('body').on('click', '.vtexIdUI-page .close', () => {
       window.location.href = '/';
     });
   }
+  vtexjs.checkout.getOrderForm().done((orderForm) => {
+    const ifLogged = orderForm.loggedIn;
+    if (ifLogged) {
+      // MOBILE
+      const emailLimited = orderForm.clientProfileData.email.length;
+      const elementWelcomeMobile = orderForm.clientProfileData.email;
+      if (emailLimited > 15) {
+        console.log(elementWelcomeMobile);
+
+        $('.menu__mobile-welcome').text(`Bem Vindo,${elementWelcomeMobile.substr(0, 18)}...`);
+      }
+
+      //   DESKTOP
+
+      const elementWelcome = `<a href="/account">Bem Vindo, ${
+        orderForm.clientProfileData.email
+      }</a>`;
+      $('.tip-bar__login').html(elementWelcome);
+    }
+  });
 });
