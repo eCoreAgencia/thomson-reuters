f2cf718ac46291e436d9f5daf8b16be23281e7fe
Lucas Dórea <lucasfdorea@gmail.com>
2019-08-02 16:33:59 -0300

    Ajustes menu mobile e login mobile

--- src/js/components/menuMobile.js
+++ src/js/components/menuMobile.js
@@ -6,9 +6,15 @@ $(document).ready(function(){
 		_this.next().toggleClass('active');
 	})
 
+	$('li.menu__mobile-items .menuItem__title').on('click', function (e) {
+		$('li.menu__mobile-items .titleMenu').toggleClass('active');
+		$('li.menu__mobile-items .submenu').toggleClass('active');
+	})
+
 	const menuIcon = $(".header__menu-icon");
 	const menuMobileContent = $(".menu__mobile");
 	const bodyChange = $('body');
+	const overlay = $(".menu__mobile-overlay");
 
 	const toggleTheMenu = () => {
 		menuMobileContent.toggleClass("menu__mobile--is-visible");
@@ -16,6 +22,9 @@ $(document).ready(function(){
 		bodyChange.toogleClass('menu-active');
 	}
 	menuIcon.on('click', function(){
+		toggleTheMenu();
+	});
+	overlay.on('click', function(){
 		toggleTheMenu();
 	});
 })
